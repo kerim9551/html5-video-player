8a385e102d3bb2a1dcbd0b0ac82743198d07580a
Kerim Garlyyev <kerim9551@gmail.com>
2022-03-26 17:58:34 +0500

    added time and volume

--- js/video.js
+++ js/video.js
@@ -1,7 +1,12 @@
 const videoPlayer = document.querySelector('.video-player')
 const video = videoPlayer.querySelector('.video')
 const playButton = videoPlayer.querySelector('.play-button')
+const volume = videoPlayer.querySelector('.volume')
+const currentTimeElement = videoPlayer.querySelector('.current')
+const durationTimeElement = videoPlayer.querySelector('.duration')
 
+
+//playpause
 playButton.addEventListener('click', (e) => {
 	if (video.paused) {
 		video.play()
@@ -11,3 +16,21 @@ playButton.addEventListener('click', (e) => {
 		e.target.textContent = '►'
 	}
 })
+
+//volume
+volume.addEventListener('mousemove', (e) => {
+	video.volume = e.target.value
+})
+
+//time
+const currentTime = () => {
+	let currentMinutes = Math.floor(video.currentTime / 60)
+	let currentSeconds = Math.floor(video.currentTime - currentMinutes * 60)
+	let durationMinutes = Math.floor(video.duration / 60)
+	let durationSeconds = Math.floor(video.duration - durationMinutes * 60)
+
+	currentTimeElement.innerHTML = `${currentMinutes}:${currentSeconds < 10 ? '0' + currentSeconds : currentSeconds}`
+	durationTimeElement.innerHTML = `${durationMinutes}:${durationSeconds}`
+}
+
+video.addEventListener('timeupdate', currentTime)
